b4c894dfeee3f5b172d28ceb758b09da328352bc
cseMisbah <mijbahsakib@gmial.com>
2025-02-15 13:54:19 +0600

    nested list done

--- REACT-JS-PRACTICE/src/App.jsx
+++ REACT-JS-PRACTICE/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+
+const users = [
+  {
+    fullName: "Anisul Islam",
+    age: 32,
+    phones: [{ home: "093585" }, { office: "347598357" }],
+  },
+  {
+    fullName: "David hunt",
+    age: 49,
+    phones: [{ home: "093585" }, { office: "347598357" }],
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Nested lists</h1>
+      {users.map((user, index) => (
+        <article key={index}>
+          <h3>Fullname :{user.fullName}</h3>
+          <p>Age : {user.age}</p>
+          user.phones.map((phone,index) =>{" "}
+          <div>
+            <p>Home : {phone.home}</p>
+            <p>Office : {phone.office}</p>
+          </div>{" "}
+          )
+        </article>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
